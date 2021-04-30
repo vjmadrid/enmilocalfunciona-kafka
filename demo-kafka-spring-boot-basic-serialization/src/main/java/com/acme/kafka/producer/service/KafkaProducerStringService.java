3b0e1f158091cbc2dda962c50fb9c0018a80e828
vjmadrid <vjmadrid@atsistemas.com>
2021-04-30 08:33:25 +0200

    Delete Future Callback

--- demo-kafka-spring-boot-basic-serialization/src/main/java/com/acme/kafka/producer/service/KafkaProducerStringService.java
+++ demo-kafka-spring-boot-basic-serialization/src/main/java/com/acme/kafka/producer/service/KafkaProducerStringService.java
@@ -6,12 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.ListenableFutureCallback;
-
-import com.acme.kafka.producer.callback.CustomListenableFutureCallback;
 
 @Service
 public class KafkaProducerStringService {
@@ -42,32 +37,6 @@ public class KafkaProducerStringService {
     public void send(String topic, ProducerRecord<String, String> record){
     	LOG.info("[KafkaProducerStringService] sending message='{}' to topic param='{}'",record.value(),topic);
     	kafkaTemplateString.send(record);
-    }
-    
-    public void sendWithCallbackAdhoc(String topic, ProducerRecord<String, String> record){
-    	LOG.info("[KafkaProducerStringService] sending message='{}' to topic param='{}'",record.value(),topic);
-    	
-    	ListenableFuture<SendResult<String, String>> future =kafkaTemplateString.send(record);
-    	
-    	future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
-            @Override
-            public void onFailure(Throwable ex) {
-            	LOG.info("[Callback] Unable to send message=[{}] due to : {}", record.value(), ex.getMessage());
-            }
-
-            @Override
-            public void onSuccess(SendResult<String, String> result) {
-            	LOG.info("[Callback] Sent message=[{}] with offset=[{}]", record.value(), result.getRecordMetadata().offset());
-            }
-        });
-    }
-    
-    public void sendWithCallbackClass(String topic, ProducerRecord<String, String> record){
-    	LOG.info("[KafkaProducerStringService] sending message='{}' to topic param='{}'",record.value(),topic);
-    	
-    	ListenableFuture<SendResult<String, String>> future =kafkaTemplateString.send(record);
-    	
-    	future.addCallback(new CustomListenableFutureCallback());
     }
     
 }
